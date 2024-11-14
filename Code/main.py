ea166b5a2ddd2d9c1d9f10b5bfc340d96be96276
Ali-Hasan-Khan28 <alihasank2003@gmail.com>
2024-11-14 05:45:30 +0500

    All app is built in main.py, only chatbot functionality is left

--- Code/main.py
+++ Code/main.py
@@ -2,12 +2,12 @@ import pandas as pd
 from chabot import unique_ids_in_resultschatbot
 from genetic_algorithm import genetic_algorithm
 
-input_data = pd.read_csv("csv_dataframe - Sheet1.csv")
+course_metadata = pd.read_csv("csv_dataframe - Sheet1.csv")
 result_chabot = pd.read_csv("results_chatbot.csv")
 
 columns = [f'lh{i}' for i in range(15)]
 data = [[None for _ in range(15)] for _ in range(5)]
 timetable = pd.DataFrame(data, columns=columns)
 
-genetic_algorithm(input_data,result_chabot,unique_ids_in_resultschatbot)
+print(genetic_algorithm(course_metadata,result_chabot,unique_ids_in_resultschatbot))
 
